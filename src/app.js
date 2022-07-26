e4d2a999000df213ab73bef3ef8c2dc9f9c52a0d
lucasdozie <lucasatwon@gmail.com>
2022-07-26 18:25:31 +0100

    first commit of main

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const compression = require("compression");
 const helmet = require("helmet");
 
@@ -8,30 +9,17 @@ app.use(compression)
 
 app.use(helmet()) //protect the header
 
-app.use("api/v1", (req, res, next) => {
-    return res.status(200).json({
-        status: "success",
-        message: "Welcome to dnuemarol !!!"
-    })
-});
+app.use(cors());
 
-//app.get("api/v1/testCpus", (req, res, next) => {
-    const router = app.route() 
-router.get("/testCpus", (req, res, next) => {
-    let n = parseInt(req.query.n);
-    let count = 0;
-     
-    if (n > 5000000000) n = 5000000000;
-     
-    for (let i = 0; i <= n; i++) {
-      count += i;
-    }
-     
-    return res.status(200).json({
-        status: "success",
-        message: "Welcome to dnuemarol !!!",
-        message2: `Final count is ${count}`
-    })
-});
+// database();
+//routes
 
+
+
+// app.use("api/v1", (req, res, next) => {
+//     return res.status(200).json({
+//         status: "success",
+//         message: "Welcome to dnuemarol !!!"
+//     })
+// });
 module.exports = app
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 "use strict"
 const cluster = require("./config/cluster");
+const logger = require("./config/logger");
 const app = require("./app")
 const {PORT} = require("./config/env")
 
-cluster(() => {
-    app
-    const server = app.listen(PORT, () => {
-           // console.log(`Express listening on port: ${port}`);
-        console.log(`Worker ${process.pid} started on port ${PORT}`);
-    });  
-})
+// cluster(() => {
+//     app
+//     const server = app.listen(PORT, () => {
+//            // console.log(`Express listening on port: ${port}`);
+//         console.log(`Worker ${process.pid} started on port ${PORT}`);
+//     });  
+// })
 
-//const server = app.listen(port, () => logger.info(`Listening on port ${port}`));
+const server = app.listen(PORT, () => logger.info(`Listening on port ${PORT}`));
 
-//module.exports = server;
+module.exports = server;
